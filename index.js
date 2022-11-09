73c93f3b41e1368d6f24990a50eb5501ab9f5be2
nhn1998 <nhnayeem1@gmail.com>
2022-11-09 14:51:31 +0600

    mongo set finish

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors())
 app.use(express())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fuly7hk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('api is running')
