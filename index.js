551ea5472555f0b54aefc543ae11874e5a3ec4a1
nhn1998 <nhnayeem1@gmail.com>
2022-11-09 15:06:03 +0600

    done api call

--- index.js
+++ index.js
@@ -11,18 +11,27 @@ app.use(express())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fuly7hk.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        const servicesCollection = client.db('serviceReview').collection('services')
+        app.get('/services',async(req,res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.log(err))
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('api is running')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(port)
 })
