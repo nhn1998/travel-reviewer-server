3973070dfd601857dedea9aabd57fc2773ff15c4
nhn1998 <nhnayeem1@gmail.com>
2022-11-09 19:20:13 +0600

    id of services

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -19,6 +19,18 @@ async function run(){
             const query = {};
             const cursor = servicesCollection.find(query)
             const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const cursor = await servicesCollection.findOne(query)
+            res.send(cursor)
+        })
+        app.get('/service',async(req,res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.limit(3).toArray();
             res.send(services)
         })
     }
