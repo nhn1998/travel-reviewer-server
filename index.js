c9b3d99abea5d19348532f1725eb8f9803eecdef
nhn1998 <nhnayeem1@gmail.com>
2022-11-10 16:08:43 +0600

    delete item added successfully

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ async function run(){
             const result = await reviewCollection.insertOne(user)
             res.send(result)
         });
-        app.get('/users',async(req,res)=>{
+        app.delete('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await reviewCollection.deleteOne(query)
+            res.send(result)
+        })
+        app.get('/users/myReviews',async(req,res)=>{
             let query = {};
             if(req.query.email){
                 query = {
